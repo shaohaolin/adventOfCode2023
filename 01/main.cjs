58941307d347cd58d03511ad7baf2f4202b07b4e
Shaohao <shaohaolinca@gmail.com>
2023-12-02 07:42:15 -0800

    refactor to function

--- 01/main.cjs
+++ 01/main.cjs
@@ -5,35 +5,40 @@ console.log("filename:", filename);
 
 // Data structure: [[1, 2], [3, 8], [1, 2, 3, 4, 5], [7]];
 
-const numbers = [];
-let result = 0;
-
-// Find all numbers in each line and push them into numbers array
-file.split("\n").map((line) => {
-  const numbersInEachLine = [];
-  line.split("").map((char) => {
-    if (isNumber(char)) {
-      numbersInEachLine.push(parseInt(char, 10));
-    }
-  });
-  numbers.push(numbersInEachLine);
-});
-
-// Calculate the result
-numbers.map((numbersInEachLine) => {
-  // If there is no number in the line, skip it
-  if (numbersInEachLine.length === 0) {
-    return;
-  }
-  const firstNumber = numbersInEachLine.shift();
-  const lastNumber = numbersInEachLine.pop();
-
-  const sum = firstNumber * 10 + (lastNumber ?? firstNumber);
-  result += sum;
-});
-
-console.log(result);
-
 function isNumber(char) {
   return !isNaN(Number(char));
 }
+
+function extractNumbersFromString(string) {
+  const numbers = [];
+  string.split("").map((char) => {
+    if (isNumber(char)) {
+      numbers.push(parseInt(char, 10));
+    }
+  });
+  return numbers;
+}
+
+// Calculate the result
+function calculateResult(numbers) {
+  let result = 0;
+
+  numbers.map((numbersInEachLine) => {
+    // If there is no number in the line, skip it
+    if (numbersInEachLine.length === 0) {
+      return;
+    }
+    const firstNumber = numbersInEachLine.shift();
+    const lastNumber = numbersInEachLine.pop();
+
+    const sum = firstNumber * 10 + (lastNumber ?? firstNumber);
+    result += sum;
+  });
+
+  return result;
+}
+
+// Find all numbers in each line and push them into numbers array
+const numbers = file.split("\n").map((line) => extractNumbersFromString(line));
+const result = calculateResult(numbers);
+console.log("result:", result);
